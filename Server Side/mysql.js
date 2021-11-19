7d7aa626c272b59ea32665db9c4f883ac48efad2
FilaMarek <48298031+FilaMarek@users.noreply.github.com>
2021-11-19 16:11:12 +0100

    deleting sensitive data

--- Server Side/mysql.js
+++ Server Side/mysql.js
@@ -1,20 +1,13 @@
 var mysql = require("mysql");
 
 const con = mysql.createConnection({
-    host: 'us-cdbr-east-02.cleardb.com',
-    user: 'b4546435e0b9bc',
-    password: 'aaadbfe5',
-    database: 'heroku_5ebefb60c94ae76'
+
 
 });
 
 /*
 var pool = mysql.createPool({
-    connectionLimit: 10, // default = 10
-    host: 'us-cdbr-east-02.cleardb.com',
-    user: 'b4546435e0b9bc',
-    password: 'aaadbfe5',
-    database: 'heroku_5ebefb60c94ae76'
+
 */
 
 module.exports.QueryDB = () => {
